8976cab9c63020a134efdb72f45a578829c39152
Piotr Rokita <piotr.m.rokita@gmail.com>
2019-06-22 12:56:48 +0200

    userModel add

--- tweeter/src/main/java/pl/tweeter/app/model/User.java
+++ tweeter/src/main/java/pl/tweeter/app/model/User.java
@@ -1,4 +1,25 @@
 package pl.tweeter.app.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private LocalDateTime createTimestamp;
+
+    private String login;
+
+    private String password;
+
+    private String role;
+
+    private Integer userDescriptionId;
+
 }
